3db846769c3ce28ec234bf62a16e6c233dced313
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-05-28 23:08:13 +0900

    Post user

--- server/server.js
+++ server/server.js
@@ -1,2 +1,26 @@
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+
 let {mongoose} = require('./db/mongoose');
 let {User} = require('./models/user');
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/users', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  let user = new User(body);
+
+  user.save().then((user) => {
+    res.send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started up server');
+});
+
+module.exports = {app};
