355cce129803d883207b1393ed69a45c11ed6739
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-09 01:14:22 +0900

    Mlab addition

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,10 @@
 let mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Users');
+let db = {
+  localhost: 'mongodb://localhost:27017/Pw-keychain',
+  mlab: 'mongodb://kaitanuva:<takanuva1>@ds151970.mlab.com:51970/pw-keychain'
+};
+
+mongoose.connect(db.localhost || db.mlab);
 
 module.exports = {mongoose};
