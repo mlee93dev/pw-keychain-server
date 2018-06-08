180cd6f3d62553bd2ba7f32c2b3627ed32e6c7b0
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-09 01:56:40 +0900

    more mlab fixes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ let mongoose = require('mongoose');
 
 let db = {
   localhost: 'mongodb://localhost:27017/Pw-keychain',
-  mlab: 'mongodb://kaitanuva:<takanuva1>@ds151970.mlab.com:51970/pw-keychain'
+  mlab: 'mongodb://kaitanuva:takanuva1@ds151970.mlab.com:51970/pw-keychain'
 };
 
-mongoose.connect(process.env.PORT? db.mlab : db.localhost);
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
-module.exports = {mongoose};
+module.exports = {mongoose}
