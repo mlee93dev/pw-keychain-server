374be8d28d49735e222ebe100217b654f9e8dac2
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-10 17:52:06 +0900

    cors all

--- server/server.js
+++ server/server.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://protected-woodland-32658.herokuapp.com');
+  // res.header('Access-Control-Allow-Origin', 'https://protected-woodland-32658.herokuapp.com');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 })
