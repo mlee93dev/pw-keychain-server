fbab04a2295ed4aad97a09ab132e29996e212e4d
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-15 14:02:11 +0900

    login error

--- server/server.js
+++ server/server.js
@@ -40,7 +40,7 @@ app.post('/users/login', async (req, res) => {
     res.header('x-auth', token).send(user);
   }
   catch (e) {
-    res.status(400).send();
+    res.status(400).send(e);
   }
 
 });
