9bca3e6c94249d31756334b99baabaa904586206
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-06-19 11:37:29 +0900

    CORS methods

--- server/server.js
+++ server/server.js
@@ -13,8 +13,9 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
   // res.header('Access-Control-Allow-Origin', 'https://protected-woodland-32658.herokuapp.com');
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Expose-Headers', 'X-Auth')
+  res.header('Access-Control-Expose-Headers', 'X-Auth');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'POST, DELETE');
   next();
 })
 
