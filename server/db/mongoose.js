f45fb2bdc66e169ae906136cc17ec41f8d6e08cd
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-07-01 10:40:48 +0900

    new db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 
 let db = {
   localhost: 'mongodb://localhost:27017/Pw-keychain',
-  mlab: 'mongodb://markdev:takanuva1@ds151970.mlab.com:51970/pw-keychain'
+  mlab: 'mongodb://markdev:takanuva1@ds125001.mlab.com:25001/pw-keychain'
 };
 
 mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
