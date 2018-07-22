84dd6a257411805a8928ec48dc089ff1d88100d4
Mark Lee <marklee@Marks-MacBook-Air.local>
2018-07-22 17:23:30 +0900

    access control allow origin set to client site on heroku

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://protected-woodland-32658.herokuapp.com/');
   res.header('Access-Control-Expose-Headers', 'x-auth');
   res.header('Access-Control-Allow-Headers', 'Origin, x-auth, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'POST, DELETE, OPTIONS');
